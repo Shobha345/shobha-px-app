33a3cd40df23b2916477d948ae2b97bef32cf2b6
sai24kumar24 <sai24kumar24@gmail.com>
2022-04-08 19:19:02 +0530

    user id config

--- js/login.js
+++ js/login.js
@@ -5,15 +5,38 @@
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-GBHANBHUKHYZ-2");
 
+
+
 var useremail = 'demouser@gmail.com';
 
 function checkIfValidUser() {
     var user = document.getElementById('email');
     if(user.value === useremail)
     {
+        //passing user and account objects //works only after apt.init
+        aptrinsic("identify",
+        {
+        //User Fields
+        "id": "basic1234" + useremail, // Required for logged in app users
+        "email": useremail,
+        "firstName": "John",
+        "lastName": "Smith",
+        "signUpDate": 1522697426479, //unix time in ms
+        "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+        "price" : 95.5,
+        "userHash": "" // optional transient for HMAC identification
+        },
+        {
+        //Account Fields
+        "id":"GSPX", //Required
+        "name":"Gainsight",
+        "Program": "Platinum" // flat custom attributes
+        });
+
         alert('There you go!');
         //route to homepage.html
         location.replace("homepage.html");
+
     }
     else
     {
