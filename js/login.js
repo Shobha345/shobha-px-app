410acb99ceb509c828b53b88c4929ad29fdeeadf
sai24kumar24 <sai24kumar24@gmail.com>
2022-04-12 14:20:58 +0530

    custom attr and multiple logins

--- js/login.js
+++ js/login.js
@@ -10,39 +10,91 @@
 var useremail = 'demouser@gmail.com';
 
 function checkIfValidUser() {
-    var user = document.getElementById('email');
-    if(user.value === useremail || user.value == 'saikumar@gmail.com' || user.value == 'sai24@gmail.com' || user.value == 'kumar@gmail.com'|| user.value == 'integration@gmail.com')
-    {
-        //passing user and account objects //works only after apt.init
-        aptrinsic("identify",
-        {
-        //User Fields
-        "id": "basic12345" + useremail, // Required for logged in app users
-        "email": useremail,
-        "firstName": "Sai",
-        "lastName": "Padala",
-        "role": "PX Analyst",
-        "signUpDate": 1522697426479, //unix time in ms
-        "plan" : "Premium", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-        "price" : 95.5,
-        "userHash": "" // optional transient for HMAC identification
-        },
-        {
-        //Account Fields
-        "id":"GSPX", //Required
-        "name":"Gainsight",
-        "Program": "Platinum", // flat custom attributes
-        "Subscription" : "Trial"
-        });
-
-        alert('There you go!');
-        //route to homepage.html
-        location.replace("homepage.html");
-
+    // var user = document.getElementById('email');
+    var user = document.getElementById('email').value;
+    var b;
+    const companyid = {
+      i1: "GS-PX",
+      i2: "GS-CS",
+      i3: "GS-RO"
     }
+    const companyname = {
+      n1: "Product Experience",
+      n2: "Customer Success",
+      n3: "Revenue Optimization"
+    }
+
+    if(user!="")
+    {
+      if(user === useremail || user == 'saikumar@gmail.com' || user == 'sai24@gmail.com' || user == 'kumar@gmail.com'|| user == 'integration@gmail.com')
+      {
+        alert('There you go!');
+        b = user.substr(8, 5);
+        var id = b;
+        if(user == 'saikumar@gmail.com')
+        {
+            aptrinsic("identify",
+            {
+            //User Fields
+            "id": "basic12345" + useremail,
+            "email": useremail,
+            "firstName": "Sai",
+            "lastName": "Kumar",
+            "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+            },
+            {
+            //Account Fields
+            "id": companyid.i1, 
+            "name": companyname.n1
+            });
+        } 
+        else if(user == 'sai24@gmail.com')
+        {
+            aptrinsic("identify",
+            {
+            //User Fields
+            "id": id,
+            "email": useremail,
+            "firstName": "Sai",
+            "lastName": "Padala",
+            "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+            },
+            {
+            //Account Fields
+            "id": companyid.i2, 
+            "name": companyname.n2
+            });
+        }
+        else if(user == 'kumar@gmail.com')
+        {
+            aptrinsic("identify",
+            {
+            //User Fields
+            "id": id,
+            "email": useremail,
+            "firstName": "Kumar",
+            "lastName": "Padala",
+            "subscriptionId": "e2cdcaad-5fb9-4239-92a0-1504d5e79b3c"
+            },
+            {
+            //Account Fields
+            "id": companyid.i3, 
+            "name": companyname.n3
+            });
+        }
+          
+      }
+      //route to homepage.html
+      location.replace("homepage.html");
+    } 
     else
     {
         alert('Invalid user, please use "demouser@gmail.com" to login');
     }
 }
 
+//clearing cookies
+function deleteAllCookies() {
+  window.aptrinsic('reset');
+  // counter = 0;
+}
